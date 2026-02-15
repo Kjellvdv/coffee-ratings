d84f40dcf954cef0c247900b8ad7a7e8a944c356
Coffee Rater Deploy <deploy@coffee-rater.app>
2026-02-15 10:00:55 +0100

    update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'drizzle-kit';
-
-export default {
-  schema: './shared/schema.ts',
-  out: './drizzle',
-  driver: 'pg',
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
-  },
-} satisfies Config;
+import type { Config } from 'drizzle-kit';                                                               
+                                                                                                           
+  export default {
+    schema: './shared/schema.ts',                                                                          
+    out: './drizzle',                                                                                      
+    dialect: 'postgresql',                                                                                 
+    dbCredentials: {
+      url: process.env.DATABASE_URL!,
+    },
+  } satisfies Config;
